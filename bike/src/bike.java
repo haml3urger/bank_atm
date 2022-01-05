6798b108907986cf175c147fb563ba9a2d87a3a4
Subham Thapa <85979451+haml3urger@users.noreply.github.com>
2022-01-05 14:03:04 +0545

    Add files via upload

---
+++ bike/src/bike.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class bike{
+    public static void main(String[] args) {
+        Scanner car = new Scanner(System.in);
+        String moose = car.nextLine();
+        System.out.println(moose);
+    }
+}
